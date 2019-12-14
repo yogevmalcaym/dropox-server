a9497bd070e17b2357efe5849b773beb59a76991
yogevmalcaym <yogevmalcaym@gmail.com>
2019-12-14 15:09:42 +0200

    websockets implementation improved

--- index.js
+++ index.js
@@ -1,76 +1,56 @@
 #!/usr/bin/env node
 const path = require("path");
 const config = require("./config.json");
+const net = require("net");
 
+const wsServer = new net.Server();
+const PORT = config.port;
+const HOST = config.host;
+wsServer.listen({ port: PORT, host: HOST });
 global.sharedFolderPath = path.join(__dirname, config.sharedFolderName);
 
-const wsServer = require("./services/wsServer").wsServer;
 const utils = require("./shared/utils");
 const messageHandlers = require("./services/messageHandlers");
-const files = require("./services/files");
 const Client = require("./services/Client");
 
-const wsServerSession = new wsServer().session;
-
-// returns true to accept the connection only ifthe origin is allowed
-// @param `origin` string
-const originIsAllowed = origin => {
-	//Check if the origin is allowed
-	const allowed = config.allowedOrigins.includes(origin) ? true : false;
-	//TODO need to check how do I get the origin
-	//may not be here since origin accepted only if the request is from a browser.
-	//**FOR NOW ALLOW ALWAYS**
-	return true;
-};
-
-// Handles request for a connection from a client.
-const requestHandle = request => {
+wsServer.on("listening", () => {
+	const { port, family, address } = wsServer.address();
+	console.log(
+		`server is listening on port: ${port} family: ${family} address: ${address}`
+	);
+});
+wsServer.on("connection", socket => {
 	let client;
-	// Make sure only allowed origin accepted
-	if (!originIsAllowed(request.origin)) {
-		request.reject();
-		console.log(
-			new Date() + " Connection from origin " + request.origin + " rejected."
-		);
-		return;
-	}
-
-	// Handle message received event.
-	// Routes the received message to the appropriate function handler, and send back to
+	// Handles data event.
+	// Routes the received data to the appropriate function handler, and sends back to
 	// the client the handler response.
-	// @param message {object} -> using `utf8Data` property.
-	const messageReceivedHandle = message => {
-		const { type: messageType, ...restArgs } = utils.stringToJSON(
-			message.utf8Data
-		);
+	// @param data {string} -> utf8 encoded.
+	const dataReceivedHandler = data => {
+		const { type, ...restArgs } = utils.stringToJSON(data);
 
 		// Operates the appropriate function handle case with the arguments that received from the client.
-		// Client instance is used only in `command` and `mainClientFolder` type messages.
-		const response = messageHandlers[messageType](restArgs, client);
+		// Client instance is used only in `command` and `mainClientFolder` types.
+		const response = messageHandlers[type](restArgs, client);
 
-		// For the first question's answer.
-		if (messageType === "mainClientFolder")
+		// For the first question's answer - create the client instance.
+		if (type === "mainClientFolder")
 			if (!client) client = new Client(restArgs.folderName);
 
-		if (response) connection.sendUTF(utils.JSONToString(response));
+		if (response) socket.write(utils.JSONToString(response));
 	};
 
-	// Handles connection closed event.
-	// @param `reasonCode` {number} -> -1 if connection still exists.
-	// @param description {string}.
-	const connectionClosedHandle = (reasonCode, description) => {
-		if (connection && connection.folderName)
-			//TODO if its empty, delete it.
-			files.getFolderData(connection.folderName);
-		console.log(new Date() + " Peer " + " disconnected.");
+	// Handles error event.
+	// @param error {object}
+	const errorHandler = error => {
+		//TODO make sure that the client didnt left the folder without password.
+		if (error.code === "ECONNRESET") console.log("Client has disconnected");
+		else console.error(error);
 	};
 
-	//returns the established WebSocketConnection.
-	const connection = request.accept("echo-protocol");
-
-	console.log(new Date() + " Connection accepted.");
-	connection.on("message", messageReceivedHandle);
-	connection.on("close", () => connectionClosedHandle());
-};
-
-wsServerSession.on("request", requestHandle);
+	socket.setEncoding("utf8");
+	console.log(
+		"connection request arrived from client: " + socket.remoteAddress
+	);
+	socket.on("error", errorHandler);
+	socket.on("data", dataReceivedHandler);
+});
